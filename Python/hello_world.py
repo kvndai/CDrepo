90bec3e3a2fe242fbecf89d6ceab7a1d7d87494e
Kevin Dai <kevin.t.dai@gmail.com>
2017-06-05 12:59:01 -0700

    pythonfun

---
+++ Python/hello_world.py
@@ -0,0 +1,20 @@
+# print "this is a sample string"
+
+# name = "Zen";
+# print "My name is " +name
+
+# name = "Zen";
+# print "My name is " + 25 #cannot concatinate str and int
+
+# first_name = "Zen"
+# last_name = "Coder"
+# print "My name is {} {}".format(first_name,last_name);
+
+# hw = "hello %s" % 'world'
+# print hw
+# # the output would be:
+# # hello world
+
+str = "this is string example... wow!!!";
+sub = "is"
+print str.endswith(sub,2,7)
