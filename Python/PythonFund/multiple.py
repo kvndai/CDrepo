09522e49bf05f1e6a9d2aae2498003b57eb9b16d
Kevin Dai <kevin.t.dai@gmail.com>
2017-06-05 18:03:22 -0700

    python

---
+++ Python/PythonFund/multiple.py
@@ -0,0 +1,22 @@
+# Multiples part 1
+# for i in range(0, 1001):
+#     if i%2 == 1:
+#         print i
+
+# Part 2
+# for j in range(5,1000001,5):
+#     print j
+
+# Sum List
+# a = [1, 2, 5, 10, 255, 3]
+# sum = 0
+# for i in a:
+#     sum += i
+# print sum
+
+# Average List
+a = [1, 2, 5, 10, 255, 3]
+sum = 0
+for i in a:
+    sum += i
+print sum/len(a)
