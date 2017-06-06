00889602566e0559c0301355428a77be6700c862
Kevin Dai <kevin.t.dai@gmail.com>
2017-06-06 10:40:12 -0700

    functions

--- Python/PythonFund/multitable.py
+++ Python/PythonFund/multitable.py
@@ -3,9 +3,9 @@ for x in range(13):
     rowStr += str(x)
 print rowStr
 
-# for y in range(1,13):
-#     prodStr = ""
-#     prodStr += str(y)
-#     for k in range(1,13):
-#         prodStr += str(k * y)
-#     print prodStr
+for y in range(1,13):
+    prodStr = ""
+    prodStr += str(y)
+    for k in range(1,13):
+        prodStr += str(k * y)
+    print prodStr
