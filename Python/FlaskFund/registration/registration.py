eec94ed675d151b0c2f9091d9530faf29ea7a059
kvndai <kevin.t.dai@gmail.com>
2017-06-11 16:38:22 -0700

    registration

--- Python/FlaskFund/registration/registration.py
+++ Python/FlaskFund/registration/registration.py
@@ -15,24 +15,47 @@ def process():
     session['lname'] = request.form['lname']
     session['password'] = request.form['password']
     session['confpw'] = request.form['confpw']
-    if len(session['email']) < 1 and len(session['fname']) < 1 and len(session['lname']) < 1 and len(session['password']) < 1 and en(session['confpw']) < 1:
+    if len(session['email']) < 1 and len(session['fname']) < 1 and len(session['lname']) < 1 and len(session['password']) < 1 and len(session['confpw']) < 1:
         flash('Email cannot be blank!')
         flash('First Name cannot be blank!')
         flash('Last Name cannot be blank!')
         flash('Password cannot be blank!')
         flash('Confirm your Password cannot be blank!')
+        return redirect('/')
     if len(session['email']) < 1:
         flash('Email cannot be blank!')
+        return redirect('/')
+    elif not EMAIL_REGEX.match(request.form['email']):
+        flash("Invalid Email Address!")
     if len(session['fname']) < 1:
+        flash('First Name cannot be blank!')
+        return redirect('/')
+    elif session['fname'].isalpha() == False:
+        flash('First name must only contain alphabet characters')
+        return  redirect('/')
+    if len(session['lname']) < 1:
+        flash('Last Name cannot be blank!')
+        return redirect('/')
+    elif session['lname'].isalpha() == False:
+        flash('Last name must only contain alphabet characters')
+        return redirect('/')
+    if len(session['password']) < 1:
+        flash('Password cannot be blank!')
+        return redirect('/')
+    elif len(session['password']) >= 8:
+        flash('Password cannot be more than 8 characters')
+        return redirect('/')
+    if len(session['confpw']) < 1:
+        flash('Confirm your Password cannot be blank!')
+        return redirect('/')
+    elif session['password'] != session['confpw']:
+        flash('Passwords must match')
+        return redirect('/')
+    return redirect('/success')
 
-
-
-
-
-    return redirect('/')
-
-@app.route('/success', methods=['POST'])
+@app.route('/success', methods=['GET'])
 def success():
+    flash('Thank you for submitting')
     return redirect('/')
 
 app.run(debug=True)
