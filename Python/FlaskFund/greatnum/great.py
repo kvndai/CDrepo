cc2661f394e2ac97f3431afd4bc0a902b9ca201b
kvndai <kevin.t.dai@gmail.com>
2017-06-10 22:36:36 -0700

    gold:

--- Python/FlaskFund/greatnum/great.py
+++ Python/FlaskFund/greatnum/great.py
@@ -1,29 +1,43 @@
-from flask import Flask, flash, render_template, request, redirect, session
+from flask import Flask, render_template, request, redirect, session
 import random
 app = Flask(__name__)
-app.secret_key = "NumberGame101"
+app.secret_key = "thisissecret"
+
+
 @app.route('/')
-def num_game():
-    try:
-        session['random']
-    except:
-        session['random'] = random.randint(1, 101)
+def index():
     return render_template('index.html')
 
-@app.route('/check', methods=['POST'])
-def check():
+@app.route('/guess', methods=['POST'])
+def guess():
+    # set the random number
+    if 'rand' not in session:
+        session['rand'] = random.randrange(1,101)
+    # print "here is the random number", session['rand']
+    # print 'here is the guess', request.form['guess']
+
     guess = int(request.form['guess'])
-    if guess > session['random']:
-        flash("Too high", 'error')
-    elif guess < session ['random']:
-        flash("Too low", 'error')
+
+    if guess < session['rand']:
+        session['guess'] = 'too low'
+
+    elif guess > session['rand']:
+        session['guess'] = 'too high'
+
     else:
-        flash("You guessed correct", 'success')
+        session['guess'] = 'correct'
+
     return redirect('/')
 
-@app.route('/reset')
+@app.route('/reset', methods=['POST'])
 def reset():
     session.clear()
     return redirect('/')
 
+
+
+# allow the user to make a guess
+# validate the guess
+# display the according box
+# reset
 app.run(debug=True)
