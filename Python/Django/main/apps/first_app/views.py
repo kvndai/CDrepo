5110add764f1162677c5637a15c2de870190b1f0
kvndai <kevin.t.dai@gmail.com>
2017-06-17 14:27:37 -0700

    portfolio

---
+++ Python/Django/main/apps/first_app/views.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+def index(request):
+    return render(request, "first_app/index.html")
+
+def show(rrequest):
+    return render(main/show_users.html
---
+++ Python/Django/portfolio/apps/portfolioapp/views.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+def index(request):
+
+    return render(request, 'portfolioapp/index.html')
+
+def testimonials(request):
+    print request.method
+    return render(request, 'portfolioapp/testimonials.html')
