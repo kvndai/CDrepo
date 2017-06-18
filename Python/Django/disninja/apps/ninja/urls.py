4c62f24b6df96a972166c854ba8df5f9c0387d25
kvndai <kevin.t.dai@gmail.com>
2017-06-18 00:43:12 -0700

    disninja

---
+++ Python/Django/disninja/apps/ninja/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+from . import views
+urlpatterns = [
+    url(r'^$$', views.index),
+    url(r'^ninja', views.ninja),
+    url(r'^ninja/(?P<color>[a-z]+)', views.ninjacolor)
+
+]
+
---
+++ Python/Django/disninja/apps/ninja/views.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render, redirect
+
+# Create your views here.
+
+def index(request):
+    return render(request, 'ninja/index.html')
+
+def ninja(request):
+    return render(request, 'ninja/ninja.html')
+
+def ninjacolor(request, color):
+    print color
+    context = {
+        'src': '/static/ninja/images/notapril.jpg'
+    }
+    ninja_color = {
+        'blue': '/static//ninja/images/leonardo.jpg',
+        'red': '/static//ninja/images/raphael.jpg',
+        'orange': '/static//ninja/images/michelangelo.jpg',
+        'purple': '/static//ninja/images/donatello.jpg'
+    }
+
+    if color in ninja_color:
+        context['src'] = ninja_color[color]
+    return render(request, 'ninja/ninjacolor.html', context)
