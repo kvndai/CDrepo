c749f7cb5d4a2f880dd1ebc772fca49f69f7c933
kvndai <kevin.t.dai@gmail.com>
2017-06-19 10:53:15 -0700

    landscape

---
+++ Python/Django/surprise/apps/surpriseme/views.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+import random
+from django.shortcuts import render, redirect
+
+# Create your views here.
+VALUES = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten']
+
+def index(request):
+    return render(request, 'surpriseme/index.html')
+
+def process(request):
+    if request.method == 'POST':
+        random.shuffle(VALUES)
+        request.session['a'] = int(request.POST['number'])
+        return redirect('/results')
+    return redirect('/')
+
+def results(request):
+    context = {
+        'pp': VALUES[: request.session['a']]
+    }
+    return render(request, 'surpriseme/results.html', context)
