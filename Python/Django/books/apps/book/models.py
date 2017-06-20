4f1cd7b0b2aad50450584f1b741d50512125f6d3
kvndai <kevin.t.dai@gmail.com>
2017-06-20 13:52:33 -0700

    books

--- Python/Django/books/apps/book/models.py
+++ Python/Django/books/apps/book/models.py
@@ -4,3 +4,8 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=30)
+    author = models.CharField(max_length=30)
+    publishdate = models.CharField(max_length=30)
+    category = models.CharField(max_length=30)
--- Python/Django/books/apps/book/views.py
+++ Python/Django/books/apps/book/views.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+from .models import Book
 from django.shortcuts import render
 
+
 # Create your views here.
+def index(request):
+
+    return render(request, 'book/index.html')
