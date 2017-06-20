49e2f3a579a3ecbc4e3a78505fde52ecc152f674
kvndai <kevin.t.dai@gmail.com>
2017-06-20 16:57:12 -0700

    wall

--- Python/Django/books/apps/book/models.py
+++ Python/Django/books/apps/book/models.py
@@ -4,8 +4,12 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Author(models.Model):
+    name = models.CharField(max_length=255)
+    age = models.IntegerField()
+
 class Book(models.Model):
     title = models.CharField(max_length=30)
-    author = models.CharField(max_length=30)
     publishdate = models.CharField(max_length=30)
     category = models.CharField(max_length=30)
+    author = models.ForeignKey(Author)
--- Python/Django/books/apps/book/views.py
+++ Python/Django/books/apps/book/views.py
@@ -1,10 +1,25 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-from .models import Book
+from .models import Book, Author
 from django.shortcuts import render
 
 
 # Create your views here.
 def index(request):
+    # one = Author.objects.create(name='One', age=20)
+    # two = Author.objects.create(name='Two', age=21)
+    # three = Author.objects.create(name='Three', age=22)
+    # four = Author.objects.create(name='Four', age=23)
+    # five = Author.objects.create(name='Five', age=24)
+    #
+    # a = Book.objects.create(title='A', publishdate='1999', category='drama', author=one)
+    # b = Book.objects.create(title='B', publishdate='1998', category='comedy', author=one)
+    # c = Book.objects.create(title='C', publishdate='1997', category='scifi', author=four)
+    # d = Book.objects.create(title='D', publishdate='1996', category='action', author=four)
+    # e = Book.objects.create(title='E', publishdate='1995', category='documentary', author=four)
+
+
+    for i in Book.objects.all():
+        print i.author.name
 
     return render(request, 'book/index.html')
