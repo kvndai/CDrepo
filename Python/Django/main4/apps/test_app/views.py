9982c4f6ca83c9e9b598508ef940e9af14997e38
kvndai <kevin.t.dai@gmail.com>
2017-06-21 10:16:02 -0700

    fullstackd

---
+++ Python/Django/main4/apps/test_app/views.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from .models import Blog, Comment
+from django.shortcuts import render, redirect
+
+# Create your views here.
+def index(request):
+    context = {
+        'blah' : Blog.objects.all()
+
+    }
+    return render(request, 'test_app/index.html', context)
+
+def blogs(request):
+    # using ORM
+    Blog.objects.create(title=request.POST['title'], blog=request.POST['blog'])
+    # INSERT INTO class Blog (title, blog, created_at, updated_at) VALUES (title, blog, created_at, updated_at)
+
+    return redirect('/')
