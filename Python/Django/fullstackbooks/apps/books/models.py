5a15ae9745507d7098815fa99c1888b20cc2ba36
kvndai <kevin.t.dai@gmail.com>
2017-06-21 14:55:23 -0700

    courses

--- Python/Django/fullstackbooks/apps/books/models.py
+++ Python/Django/fullstackbooks/apps/books/models.py
@@ -4,3 +4,9 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=30)
+    author = models.CharField(max_length=30)
+    category = models.CharField(max_length=30)
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
--- Python/Django/fullstackbooks/apps/books/views.py
+++ Python/Django/fullstackbooks/apps/books/views.py
@@ -1,6 +1,15 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
-from django.shortcuts import render
+from .models import Book
+from django.shortcuts import render, redirect
 
 # Create your views here.
+def index(request):
+    context = {
+        'blah' : Book.objects.all()
+    }
+    return render(request, 'books/index.html', context)
+
+def process(request):
+    Book.objects.create(title=request.POST['title'], author=request.POST['author'], category=request.POST['category'])
+    return redirect('/')
