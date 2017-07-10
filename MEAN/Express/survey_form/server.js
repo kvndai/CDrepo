9005758cf2581bb71ac06e145939f476f66b1ced
kvndai <kevin.t.dai@gmail.com>
2017-07-10 13:21:19 -0700

    survey_revamped

--- MEAN/Express/survey_form/server.js
+++ MEAN/Express/survey_form/server.js
@@ -3,24 +3,36 @@ var express = require("express");
 var path = require("path");
 var app = express();
 var bodyParser = require('body-parser');
-// CONFIG
+// MIDDLEWARE
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "./static")));
 app.set('views', path.join(__dirname, './views'));
 app.set('view engine', 'ejs');
+
 // ROUTING
 app.get('/', function(req, res) {
-
  res.render("index");
 })
-
 app.post('/process', function(req, res) {
-
  console.log("POST DATA", req.body);
  res.render("result", req.body);
 })
 
 // PORT
-app.listen(8000, function() {
- console.log("listening on port 8000");
+var server  = app. listen(8000, function() {
+    console. log( "listening on port 8000");
 });
+
+// SOCKETS
+var io  =  require( 'socket.io'). listen(server);
+io.sockets. on( 'connection', function (socket) {
+    console. log( "Sockets Active");
+    console. log(socket.id);
+    // all the socket code goes in here!
+    socket. on( "posting_form", function (form_data){
+        let form_results = JSON.stringify(form_data);
+        let number = Math.floor(Math.random() * 1000) + 1;
+        socket.emit('display', form_results);
+        socket.emit('lucky', number);
+    })
+})
