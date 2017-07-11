d1a56a4dedf5e3986cf7cddea97bb3ff567e90d6
kvndai <kevin.t.dai@gmail.com>
2017-07-10 17:06:50 -0700

    chat

---
+++ MEAN/Express/chat/server.js
@@ -0,0 +1,38 @@
+// REQUIRED
+var express = require("express");
+var path = require("path");
+var app = express();
+// MIDDLEWARE
+app.use(express.static(path.join(__dirname, "./static")));
+app.set('views', path.join(__dirname, './views'));
+app.set('view engine', 'ejs');
+
+// ROUTING
+app.get('/', function(req, res) {
+ res.render("index");
+})
+
+// PORT
+var server  = app. listen(8000, function() {
+    console. log( "listening on port 8000");
+});
+
+// SOCKETS
+
+var io  =  require('socket.io'). listen(server);
+io.sockets.on('connection', function (socket) {
+    console.log("Sockets Active");
+    console.log(socket.id);
+    // all the socket code goes in here!
+    socket.on('got_new_user', function(data){
+        console.log(data);
+    })
+    // socket.on("user_click", function(){
+    //     count ++;
+    //     io.sockets.emit('number', count);
+    // })
+    // socket.on("user_reset_click", function(){
+    //     count = 0;
+    //     io.sockets.emit('reset', count);
+    // })
+})
