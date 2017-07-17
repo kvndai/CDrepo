d85d1a4f385cd2939125d99206adfc47c83a2695
kvndai <kevin.t.dai@gmail.com>
2017-07-17 11:26:59 -0700

    color

--- MEAN/Angular/first_app/first-app-app/src/app/app.component.ts
+++ MEAN/Angular/first_app/first-app-app/src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-   title = "app works";
+   email: any[] = [
+      { email: 'bill@gates.com', importance: true, subject: 'New Windows', content: 'Windows XI will launch in the year 2100' },
+      { email: 'ada@lovelace.com', importance: true, subject: 'Programming', content: 'Enchantress of Numbers' },
+      { email: 'john@carmac.com', importance: false, subject: 'Updated Algo', content: 'New algorithm for shadow volumes' },
+      { email: 'gabe@newell.com', importance: false, subject: 'HL3', content: 'Just Kidding...' }
+   ]
 }
