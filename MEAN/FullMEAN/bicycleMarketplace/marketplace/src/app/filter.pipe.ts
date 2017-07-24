ed28a9aa6a9f035aa94b831738d918ff538113bc
kvndai <kevin.t.dai@gmail.com>
2017-07-23 17:36:22 -0700

    bikes

---
+++ MEAN/FullMEAN/bicycleMarketplace/marketplace/src/app/filter.pipe.ts
@@ -0,0 +1,17 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { Bicycle } from "./bicycle";
+
+@Pipe({
+  name: 'filter'
+})
+export class FilterPipe implements PipeTransform {
+
+  transform(value: Array<Bicycle>, searchStr: string): Array<Bicycle> {
+    if (!value) { return value; }
+
+    return value.filter(bike => {
+      return bike.title.toLowerCase().includes(searchStr.toLowerCase()) || bike.description.toLowerCase().includes(searchStr.toLowerCase())
+    })
+  }
+
+}
